752641e60d47f81cdc108cb1ecc41064b3c9450e
frkylmz <farukyilmaz420@gmail.com>
2022-07-30 15:03:19 +0300

    changed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <div>faruk yılmaz</div>
+    </div>
+  );
 }
 
 export default App;
