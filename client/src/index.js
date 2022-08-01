c597341e778819dba6fcd11183983c720d87867c
frkylmz <farukyilmaz420@gmail.com>
2022-08-01 21:39:27 +0300

    add react query

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./reset.css";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
