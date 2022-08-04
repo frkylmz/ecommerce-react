3914aa91f27e88c7d11791dc5b0f1fd91b0fc892
frkylmz <farukyilmaz420@gmail.com>
2022-08-04 13:58:30 +0300

    add logout

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import { useState, createContext, useEffect, useContext } from "react";
-import { fetchMe } from "../api";
+import { fetchLogout, fetchMe } from "../api";
 
 import { Flex, Spinner } from "@chakra-ui/react";
 
@@ -29,13 +29,26 @@ const AuthProvider = ({ children }) => {
     setUser(data.user);
 
     localStorage.setItem("access-token", data.accessToken);
-    localStorage.setItem("refresh-token", data.refreshToke);
+    localStorage.setItem("refresh-token", data.refreshToken);
+  };
+
+  const logout = async (callback) => {
+    setLoggedIn(false);
+    setUser(null);
+
+    await fetchLogout();
+
+    localStorage.removeItem("access-token");
+    localStorage.removeItem("refresh-token");
+
+    callback();
   };
 
   const values = {
     loggedIn,
     user,
     login,
+    logout,
   };
 
   if (loading) {
